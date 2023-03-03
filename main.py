d041e8eb76ec935d19d8e84d029ccca7397aea4e
EgamberdiyevAbbos <pozvatff2222@gmail.com>
2023-03-03 21:33:15 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,85 @@
+
+# from Abbos_gr3_hw3 import func
+
+# func()
+# def get_tuple num
+# nums = [ ]
+# for i in str(num):
+#     nums.appened(int(i))
+# print(tuple(nums))
+#
+# def func(text, remove, replace):
+#     splitted = text.split()
+#     for elem in splitted:
+#        print(elem)
+#     if elem == remove:
+#     pass# def a():
+# #   print('a() starts')
+#   b()
+#   d()
+#   print('a() returns')
+#
+# def b():
+#    print('b() starts')
+#    c()
+#    print('b() returns')
+#
+# def c():
+
+#
+# def spam():
+#   eggs = 99
+#   bacon()
+#   print(eggs)
+#
+# def bacon():
+#   ham = 101
+#   eggs = 0
+#
+# def (' ')
+# 
+# open('../action/input.txt')
+#
+# with open('../.idea/back end') as file
+#     my_file = file.read()
+#     print(my_file)
+#     my_file = file.split()
+#    print(my_file)
+# for i in my_file:
+#     if
+#
+# with open('/tback end/back end') as f:
+#     print(f)
+#
+# class dog:
+#     def __int__(self):
+#            pass
+#     print('here')
+# fcu = dog()
+# fcu1 = dog()
+# fcu2 = dog()
+#
+# dog.__int__()
+
+
+# class Person:
+#     def __init__(self, work, age, car, name):
+#
+#         self.work = work
+#         self.age = age
+#         self.car = car
+#         self.name = name
+#
+# Aziz = Person(work = 'coder',age = '23',car = 'gentra', name = 'Aziz')
+# shokir = Person ('actor', '32','spark','shokir')
+# print(f"This is person's name {Aziz.name} her age {Aziz.age} he worked {Aziz.work} has god car type {Aziz.car} ")
+# print(shokir)
+
+class Car:
+    def __init__(self, colour, melange):
+        self.colour = colour
+        self.melange = melange
+    def __str__(self):
+        return f"My cars colour {self.colour} , Walked {self.melange}"
+colour = Car(colour= 'white', melange=20.000)
+print(colour)
